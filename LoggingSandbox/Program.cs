7726f0600a5b1af7869ce29fd647eac7755eca48
sprintup <stephen.printup@ucdenver.edu>
2023-02-03 14:25:32 -0700

    timer stuff

--- LoggingSandbox/Program.cs
+++ LoggingSandbox/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Timers;
 
 Console.WriteLine("Hello, World!");
 
@@ -21,6 +22,7 @@ public class Blah
 {
 	public void Run(ILogger<Blah> logger)
 	{
+		StartTimer("Starting");
 		Console.WriteLine("Console Writeline");
 		logger.LogTrace("here is a TRACE log");
 		logger.LogDebug("here is a DEBUG log");
@@ -28,6 +30,32 @@ public class Blah
 		logger.LogWarning("here is a WARNING log");
 		logger.LogError("here is a ERROR log");
 		logger.LogCritical("here is a CRITICAL log");
+		StopTimer();
 		Console.ReadLine();
 	}
+	#region timer
+	private static System.Timers.Timer aTimer;
+	public DateTime startTime;
+	private void StartTimer(string message)
+	{
+		startTime = DateTime.Now;
+		Console.WriteLine($"\n{message}");
+		// Create a timer with a two second interval.
+		aTimer = new System.Timers.Timer(1000);
+		// Hook up the Elapsed event for the timer. 
+		aTimer.Elapsed += OnTimedEvent;
+		aTimer.AutoReset = true;
+		aTimer.Enabled = true;
+	}
+	private void StopTimer()
+	{
+		aTimer.Stop();
+		aTimer.Dispose();
+	}
+	private void OnTimedEvent(Object source, ElapsedEventArgs e)
+	{
+		var diff = e.SignalTime - startTime;
+		Console.Write($"\rElapsed time {Math.Round(diff.TotalSeconds)}");
+	}
+	#endregion
 }
